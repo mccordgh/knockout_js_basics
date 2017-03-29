9ba24b8576d43c25937bdab41f7c55adf199db4a
Matthew McCord <mattm.eternal@gmail.com>
2017-03-29 09:26:36 -0500

    step 4 DOM data manipulation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
-function AppViewModel() {
-    this.firstName = ko.observable("Bert");
-    this.lastName = ko.observable("Bertington");
-}
+const AppViewModel = function() {
+    this.firstName = ko.observable('Bert');
+    this.lastName = ko.observable('Bertington');
+    
+    this.fullName = ko.computed(() => {
+        return `${this.lastName()}, ${this.firstName()}`;
+    }, this);
+
+    this.capitalizeLastName = () => {
+        let currentVal = this.lastName();
+        this.lastName(currentVal.toUpperCase());
+    };
+};
 
 // Activates knockout.js
 ko.applyBindings(new AppViewModel());
